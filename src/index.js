4566d4df507451f3b8790fcedafb1322a5f2c0ff
Adrian <38096991+Boobaley@users.noreply.github.com>
2018-09-29 22:38:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let result = 0;
+  let devider = 5;
+
+  if (number > 4) {
+    while (number / devider >= 1) {
+      result += Math.floor(number / devider);
+      devider = devider * 5;
+    }
+  }
+
+  return result;
 }
